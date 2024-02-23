421dec1db316cd8a5bff05e4dff65e617b0690a2
LailaCharaoui <mats2@live.fr>
2024-02-24 00:44:45 +0100

    :fire: add waiting room and redirections

--- client/src/pages/Quizz/QuizzGame.jsx
+++ client/src/pages/Quizz/QuizzGame.jsx
@@ -1,12 +1,15 @@
+import {useParams} from "react-router-dom";
 
 export const QuizzGame = () => {
     /**
      * TODO: Afficher le jeu
      */
 
+    const { roomId, category } = useParams();
+
     return (
         <div>
-            <h1>Quizz Game</h1>
+            <h1>Quizz Game {category}</h1>
         </div>
     );
 }
